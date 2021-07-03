fa306a7e189e0de387f45c96a66b575b470a0b42
mateus-f-torres <mateusfigtorres@gmail.com>
2021-07-03 19:13:55 -0300

    feat(solarized.sh): setup light/dark toggle for vim/task/tmux

---
+++ solarized.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+#https://github.com/altercation/mutt-colors-solarized
+
+vimrc="$HOME/.vimrc"
+taskrc="$HOME/.taskrc"
+tmuxrc="$HOME/.tmux.conf"
+
+main() {
+  if test $# -eq 0; then
+    # DONE: get system time
+    # TODO: get only hour segment "HH:mm:ss"
+    # TODO: setup light or dark theme based on it
+    system_time="$(date -- +%T)"
+    echo "$system_time"
+  elif test $# -eq 1; then
+    case "$1" in
+      -h|--help)
+        echo "help"
+        exit 0
+        ;;
+      -d|--dark)
+        set_theme "dark" "black" "brightblue" "brightgreen"
+        exit 0
+        ;;
+      -l|--light)
+        set_theme "light" "white" "brightyellow" "brightcyan"
+        exit 0
+        ;;
+      -?*)
+        printf "Unknown option '%s'\n" "$1"
+        exit 0
+    esac
+  fi
+}
+
+
+set_theme() {
+  # Vim
+  sed --in-place "s/background=[a-z]*/background=$1/" "$vimrc"
+  # Taskwarrior
+  sed --in-place "s/solarized-[a-z]*/solarized-$1/" "$taskrc"
+  # Tmux
+  sed --in-place "s/status-style fg=yellow,bg=[a-z]*/status-style fg=yellow,bg=$2/" "$tmuxrc"
+  sed --in-place "s/window-status-style fg=[a-z]*,bg=default/window-status-style fg=$3,bg=default/" "$tmuxrc"
+  sed --in-place "s/pane-border-style fg=[a-z]*/pane-border-style fg=$2/" "$tmuxrc"
+  sed --in-place "s/pane-active-border-style fg=[a-z]*/pane-active-border-style fg=$4/" "$tmuxrc"
+  sed --in-place "s/message-style fg=brightred,bg=[a-z]*/message-style fg=brightred,bg=$2/" "$tmuxrc"
+  sed --in-place "s/window-status-bell-style fg=[a-z]*,bg=red/window-status-bell-style fg=$2,bg=red/" "$tmuxrc"
+}
+
+main "$@"
